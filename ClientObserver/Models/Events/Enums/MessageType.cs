78e2bf7131ace6f41a241ef781e848598b413822
cregs08 <cole.regnier@gmail.com>
2024-01-30 21:22:56 +1100

    adding in comments

--- ClientObserver/Models/Events/Enums/MessageType.cs
+++ ClientObserver/Models/Events/Enums/MessageType.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+
+// Define a namespace for the ClientObserver.Models.MessageEvents
 namespace ClientObserver.Models.MessageEvents
 {
+    // Declare a public enum named 'MessageType'
+    // Enums are distinct types consisting of a set of named constants called the enumerator list.
     public enum MessageType
     {
-        Image,
-        Log,
-        Text
+        Image, // Represents a message type for images
+        Log,   // Represents a message type for logs
+        Text   // Represents a message type for text
     }
 }
-
