f4fd6e08dda0ba0e4cdae24f525cde7e0ffb5ef2
cregs08 <cole.regnier@gmail.com>
2024-02-04 17:57:23 +1100

    boolean text converter

---
+++ ClientObserver/Converters/BoolToStatusTextConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace ClientObserver.Converters
+{
+
+    public class BoolToStatusTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isConnected)
+            {
+                return isConnected ? "Connected" : "Disconnected";
+            }
+            return "Unknown";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
