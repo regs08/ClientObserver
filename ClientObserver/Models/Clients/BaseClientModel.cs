69dec0f4f8369b53edbe8a2cec1f039be8bdfc46
cregs08 <cole.regnier@gmail.com>
2024-02-10 10:34:49 +1100

    our client model classes to be used by our view models and services to connect and display data . for now jiust the mqtt one is initialized for testing

---
+++ ClientObserver/Models/Clients/BaseClientModel.cs
@@ -0,0 +1,22 @@
+﻿using ClientObserver.Configs;
+
+namespace ClientObserver.Models.Clients
+{
+    public abstract class BaseClientModel
+    {
+        // Make the setter of Name private
+        public string Name { get; private set; }
+        public bool ConnectionStatus { get; set; } = false;
+        public BaseConfig Config { get; private set; }
+
+        // Constructor to initialize Name, ConnectionStatus, and Config
+        public BaseClientModel(BaseConfig config, string name = "BaseClientModel")
+        {
+            Name = name;
+            Config = config;
+        }
+
+        // Method to assign properties from config 
+        public abstract void ApplyConfig();
+    }
+}
