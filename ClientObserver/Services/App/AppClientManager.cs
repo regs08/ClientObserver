1e6036d4054e57221125b207cf790ba15907011b
cregs08 <cole.regnier@gmail.com>
2024-02-10 16:50:23 +1100

    singleton instances that handle the server data for all servers

---
+++ ClientObserver/Services/App/AppClientManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using ClientObserver.Models.Clients;
+using ClientObserver.Models.Servers;
+// todo need to rewrite how this adds client models throwing an error when it searches for a key that doesnt exist 
+namespace ClientObserver.Managers
+{
+    public class AppClientManager
+    {
+        // ObservableCollection to store client models
+        public ObservableCollection<ServerClients> ServerClients { get; private set; }
+
+        private static AppClientManager _instance;
+        private static readonly object _lock = new object();
+
+        public static AppClientManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new AppClientManager();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private AppClientManager()
+        {
+            ServerClients = new ObservableCollection<ServerClients>();
+        }
+
+        // Method to get a client model by server name
+        public ServerClients GetClientModel(string serverName)
+        {
+            var serverClient = ServerClients.FirstOrDefault(sc => sc.ServerName == serverName);
+            if (serverClient != null)
+            {
+                return serverClient;
+            }
+            return null;
+                //throw new KeyNotFoundException($"No clients found for server: {serverName}");
+        }
+
+        // Method to add a client model
+        public void AddSeverClients(ServerClients serverClients)
+        {
+            var existingServerClient = GetClientModel(serverClients.ServerName);
+            if (existingServerClient == null)
+            {
+                ServerClients.Add(serverClients);
+            }
+            else
+            {
+                // Optionally, update the existing entry or handle duplicates as needed
+            }
+        }
+
+        // Method to remove a client model by server name
+        public void RemoveClientModel(string serverName)
+        {
+            var serverClient = GetClientModel(serverName);
+            if (serverClient != null)
+            {
+                ServerClients.Remove(serverClient);
+            }
+            else
+            {
+                // Handle the case where the server name does not exist
+            }
+        }
+    }
+}
---
+++ ClientObserver/Services/App/AppConfigManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using ClientObserver.Services;
+using ClientObserver.Models.Servers;
+
+// Class responsible for managing all the config manager instances of the app
+namespace ClientObserver.Managers
+{
+    public class AppConfigManager
+    {
+        private static AppConfigManager _instance; // The single instance
+        private List<string> _localConfigPaths;
+
+        // Class for loading configs
+        private ConfigLoader _configLoader;
+
+        private bool _isInitialized = false; // Flag to check if configs are loaded
+        // List of available configs
+        public ObservableCollection<ServerConfigs> AvailableConfigs { get; private set; }
+        // List of selected configs, dynamically updated. These are the configs we build our connections to
+        public ObservableCollection<ServerConfigs> SelectedConfigs { get; private set; }
+
+        public ObservableCollection<ServerClients> Clients { get; set; }
+        // Private constructor for singleton pattern
+        private AppConfigManager()
+        {
+            _localConfigPaths = new List<string> { "DefaultConfig.json", "GrapeModelConfig.json" };
+            _configLoader = new ConfigLoader();
+            AvailableConfigs = new ObservableCollection<ServerConfigs>();
+            SelectedConfigs = new ObservableCollection<ServerConfigs>();
+        }
+
+        // Public static method to access the singleton instance
+        public static AppConfigManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AppConfigManager();
+                }
+                return _instance;
+            }
+        }
+        public async Task InitializeAsync()
+        {
+            if (!_isInitialized)
+            {
+                await LoadLocalConfigs();
+                _isInitialized = true; // Set flag to true after loading configs for the first time
+            }
+        }
+        public async Task LoadLocalConfigs()
+        {
+            Console.WriteLine("loading local configs"); 
+            foreach (var jsonPath in _localConfigPaths)
+            {
+                ServerConfigs serverConfigs = await _configLoader.LoadConfigsFromJson(jsonPath);
+                AddtoAvailableConfigs(serverConfigs);
+            }
+        }
+        // method for add to selected 
+        public void AddToSelectedConfigs(ServerConfigs serverConfigs)
+        {
+            if (AvailableConfigs.Contains(serverConfigs))
+            {
+                if (!SelectedConfigs.Contains(serverConfigs)) // Ensure it's not already selected
+                {
+                    SelectedConfigs.Add(serverConfigs);
+                }
+            }
+        }
+
+
+
+    
+        public void AddtoAvailableConfigs(ServerConfigs serverConfigs)
+        {
+            if (!AvailableConfigs.Contains(serverConfigs))
+            {
+
+                if (serverConfigs.ValidateAllConfigs())
+                {
+                    AvailableConfigs.Add(serverConfigs);
+                }
+            }
+        }
+
+        public void RemoveFromSelectedConfigs(ServerConfigs serverConfigs)
+        {
+            if (SelectedConfigs.Contains(serverConfigs))
+            {
+                SelectedConfigs.Remove(serverConfigs);
+            }
+            else
+            {
+                Console.WriteLine("Configuration not found in selected configs.");
+            }
+        }
+
+        public void RemoveFromAvailableConfigs(ServerConfigs serverConfigs)
+        {
+            if (AvailableConfigs.Contains(serverConfigs))
+            {
+                AvailableConfigs.Remove(serverConfigs);
+            }
+            else
+            {
+                Console.WriteLine("Configuration not found in available configs.");
+            }
+        }
+
+    }
+}
+
