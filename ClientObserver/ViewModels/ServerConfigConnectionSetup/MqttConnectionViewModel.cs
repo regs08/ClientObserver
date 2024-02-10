f5fa85472976609aff6e56a06e45e9c4a5e54bad
cregs08 <cole.regnier@gmail.com>
2024-02-10 16:51:26 +1100

    adding in view to show the connection logic for connecting to an mqtt clinet

---
+++ ClientObserver/ViewModels/ServerConfigConnectionSetup/MqttConnectionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ClientObserver.Models.Clients;
+namespace ClientObserver.ViewModels.ServerConfigConnectionSetup
+{
+	public class MqttConnectionViewModel
+	{
+		public MqttConnectionViewModel(MqttClientModel clientModel)
+		{
+		}
+	}
+}
+
