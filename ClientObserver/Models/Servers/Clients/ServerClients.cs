4e7d26c2681b3031587af6f0d129887a83f54c3d
cregs08 <cole.regnier@gmail.com>
2024-02-11 19:13:54 +1100

    renaming methods

--- ClientObserver/Models/Servers/Clients/ServerClients.cs
+++ ClientObserver/Models/Servers/Clients/ServerClients.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using ClientObserver.Models.Clients;
-using ClientObserver.Helpers.Server.Clients; // Assuming ClientModelManager is placed here
+using ClientObserver.Helpers.Server.Clients;
 using System.Collections.ObjectModel;
 
 namespace ClientObserver.Models.Servers
 {
-    public class ServerClients : ServerManagerBase
+    public class ServerClients : ServerBase
     {
         private ClientModelManager clientModelManager = new ClientModelManager();
-        public ObservableCollection<BaseClientModel> ClientModels => clientModelManager.ClientModels;
+        public ObservableCollection<BaseClientModel> ClientModels => clientModelManager.Models;
 
         public ServerClients()
         {
@@ -19,26 +19,26 @@ namespace ClientObserver.Models.Servers
         public void AddClientModel(BaseClientModel clientModel)
         {
             if (clientModel == null) throw new ArgumentNullException(nameof(clientModel));
-            clientModelManager.AddClientModel(clientModel);
+            clientModelManager.AddModel(clientModel);
         }
 
         // Method to return non-null instances of BaseClientModel
-        public IEnumerable<BaseClientModel> GetNonNullClientModels()
+        public IEnumerable<BaseClientModel> GetNonNullModels()
         {
             // Leverage the ObservableCollection directly for binding or convert it to a list for other purposes
-            return clientModelManager.ClientModels;
+            return clientModelManager.Models;
         }
 
         // Method to retrieve a specific client model by type
         public T GetClientModel<T>() where T : BaseClientModel
         {
-            return clientModelManager.GetClientModel<T>();
+            return clientModelManager.GetModel<T>();
         }
 
         // Method to remove a client model by type
         public bool RemoveClientModel<T>() where T : BaseClientModel
         {
-            return clientModelManager.RemoveClientModel<T>();
+            return clientModelManager.RemoveModel<T>();
         }
     }
 }
