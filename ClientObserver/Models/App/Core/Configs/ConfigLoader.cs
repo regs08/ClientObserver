f70e13b80b2f28a6cf99a55fca48577ffb2cbdae
cregs08 <cole.regnier@gmail.com>
2024-02-18 12:29:59 +1100

    where our app level models go

---
+++ ClientObserver/Models/App/Core/Configs/ConfigLoader.cs
@@ -0,0 +1,25 @@
+﻿using ClientObserver.Models.Server.Framework.Configs;
+using ClientObserver.Services.App.Core.Configs;
+
+using System.Threading.Tasks;
+
+namespace ClientObserver.Models.App.Core.Configs
+{
+    public class ConfigLoader
+    {
+        private ConfigLoaderService _configLoaderService;
+        public List<string> LocalConfigPaths { get; private set; }
+
+
+        public ConfigLoader()
+        {
+            _configLoaderService = new ConfigLoaderService();
+            LocalConfigPaths=  new List<string> { "DefaultConfig.json", "GrapeModelConfig.json" };
+        }
+
+        public async Task<ServerConfigs> LoadConfigurationAsync(string path)
+        {
+            return await _configLoaderService.LoadConfigsFromJson(path);
+        }
+    }
+}
