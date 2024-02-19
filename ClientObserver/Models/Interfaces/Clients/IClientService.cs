874b0d7c7f75ccaaeeefef637f35bf3f49aa8e5b
cregs08 <cole.regnier@gmail.com>
2024-02-19 21:52:12 +1100

    adding in new interfaces

---
+++ ClientObserver/Models/Interfaces/Clients/IClientService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClientObserver.Models.Server.Core.Configs;
+
+namespace ClientObserver.Models.Interfaces.Clients
+{
+
+    // IClientService interface
+    public interface IClientService
+    {
+        void Connect();
+        void ApplyConfig(BaseConfig config);
+    }
+
+}
+
