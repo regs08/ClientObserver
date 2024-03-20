113d11e668c4e386a86043f4389eaf942cc0c6cf
cregs08 <cole.regnier@gmail.com>
2024-03-20 19:01:07 +1100

    Connection service iterface

---
+++ ClientObserver/Models/Interfaces/Connection/IConnectionService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ClientObserver.Models.Interfaces.Connection;
+
+/// <summary>
+/// Delegate definition for a connection step. Must return a boolean indicating the success of the step.
+/// </summary>
+public delegate Task<bool> ConnectionStep();
+
+/// <summary>
+/// Delegate definition for a disconnection step. Must return a boolean indicating the success of the step.
+/// </summary>
+public delegate Task<bool> DisconnectionStep();
+
+public interface IConnectionService
+{
+    // Properties and methods that implementing classes must provide.
+    //BaseClientModel ClientModel { get; set; }
+    Task<bool> InitializeConnection();
+    Task<bool> Authenticate();
+    Task<bool> FinalizeConnection();
+
+    Task<bool> DisconnectFromClient();
+
+    Task ConnectAsync();
+    Task DisconnectAsync();
+
+    // Use these methods or properties to expose the steps.
+    // Note: Implementing classes will have to define how these are structured.
+    ConnectionStep[] ConnectionSteps { get; }
+    DisconnectionStep[] DisconnectionSteps { get; }
+}
+
