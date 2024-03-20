2afd933a08b42af6b4febc3a8d08c6fd655633a3
cregs08 <cole.regnier@gmail.com>
2024-03-20 20:16:19 +1100

    adding in a empty view model

---
+++ ClientObserver/ViewModels/ServerConnectionSetup/ClientConnectionViewModels/VideoStream/VideoStreamConnectionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClientObserver.Models.Server.Core.Clients;
+
+namespace ClientObserver.ViewModels.ServerConnectionSetup.ClientConnectionViewModels
+{
+	public class VideoStreamConnectionViewModel: BaseConnectionViewModel
+	{
+		public VideoStreamClient VideoStreamClient => ClientModel as VideoStreamClient;
+
+		public VideoStreamConnectionViewModel(VideoStreamClient clientModel) : base(clientModel)
+		{
+		}
+	}
+}
+
