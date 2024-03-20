c7d52225d79e7ced827e3fcb43c197c74b6c3a15
cregs08 <cole.regnier@gmail.com>
2024-03-20 20:08:40 +1100

    adding in messaging service

---
+++ ClientObserver/Services/App/Messaging/MessageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ClientObserver.Models.Interfaces.Messaging;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace ClientObserver.Services.App.Messaging
+{
+    public class MessagingService : IMessagingService
+    {
+        public void Register<TRecipient, TMessage>(TRecipient recipient, Action<TRecipient, TMessage> action)
+            where TRecipient : class where TMessage : class
+        {
+            WeakReferenceMessenger.Default.Register<TRecipient, TMessage>(recipient, (recipient, message) => action(recipient, message));
+        }
+
+        public void Send<TMessage>(TMessage message) where TMessage : class
+        {
+            WeakReferenceMessenger.Default.Send(message);
+        }
+
+        public void UnregisterAll(object recipient)
+        {
+            WeakReferenceMessenger.Default.UnregisterAll(recipient);
+        }
+
+    }
+}
