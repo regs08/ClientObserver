8f1e82aa209dd5acb19b43e38df4da4533a868ad
cregs08 <cole.regnier@gmail.com>
2024-03-20 19:00:02 +1100

    custom exceptions class

---
+++ ClientObserver/Helpers/Exceptions/Exceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ClientObserver.Helpers.Exceptions
+{
+
+        public class ConnectionException : Exception
+        {
+            public  ConnectionException(string message) : base(message) { }
+        }
+
+        public class DisconnectionException : Exception
+        {
+            public DisconnectionException(string message) : base(message) { }
+        }
+
+    }
+
+
