e9addb2236a85e805a20ef2d64be6f54db40cc44
cregs08 <cole.regnier@gmail.com>
2024-03-20 18:47:20 +1100

    adding in routers

--- ClientObserver/AppShell.xaml.cs
+++ ClientObserver/AppShell.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace ClientObserver;
+﻿using ClientObserver.Views;
+using ClientObserver.Views.Display.Server;
+namespace ClientObserver;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+        Routing.RegisterRoute(nameof(MainPageTestView), typeof(MainPageTestView));
+        Routing.RegisterRoute(nameof(ServerDisplayView), typeof(ServerDisplayView));
+
+
+    }
 }
 
