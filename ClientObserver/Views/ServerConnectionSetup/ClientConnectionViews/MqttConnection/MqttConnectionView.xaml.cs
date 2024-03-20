4a395b67f2dd9fc1f9122523346e21a38dde83bb
cregs08 <cole.regnier@gmail.com>
2024-03-20 20:17:19 +1100

    connection view

---
+++ ClientObserver/Views/ServerConnectionSetup/ClientConnectionViews/MqttConnection/MqttConnectionView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using ClientObserver.ViewModels.ServerConnectionSetup.ClientConnectionViewModels;
+
+namespace ClientObserver.Views.ServerConnectionSetup.ClientConnectionViews
+{
+
+public partial class MqttConnectionView : ContentPage
+{
+	public MqttConnectionView(MqttConnectionViewModel viewModel) 
+	{
+            InitializeComponent();
+            BindingContext = viewModel;
+	}
+}
+}
