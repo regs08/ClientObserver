a34ecc8062d46b9d45e3955648ff4e121ac5027a
cregs08 <cole.regnier@gmail.com>
2024-03-20 20:18:26 +1100

    template

---
+++ ClientObserver/Views/Templates/ServerConnectionSetup/ClientConnectionViews/ClientConnectionTemplate.xaml.cs
@@ -0,0 +1,12 @@
+﻿using ClientObserver.ViewModels.ServerConnectionSetup.ClientConnectionViewModels;
+
+namespace ClientObserver.Views.Templates.ServerConnectionSetup.ClientConnectionViews;
+
+public partial class ClientConnectionTemplate : ContentView
+{
+	public ClientConnectionTemplate()
+	{
+		InitializeComponent();
+		//BindingContext = viewModel;
+	}
+}
