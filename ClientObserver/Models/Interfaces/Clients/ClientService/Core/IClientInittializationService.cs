56cb3d6a422c4ff145b73616cbe5f794c35bf876
cregs08 <cole.regnier@gmail.com>
2024-03-20 19:00:31 +1100

    interfaces for our classes

---
+++ ClientObserver/Models/Interfaces/Clients/ClientService/Core/IClientInittializationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ClientObserver.Models.Server.Core.Configs;
+
+namespace ClientObserver.Models.Interfaces.Clients.ClientService.Core
+{
+	public interface IClientInittializationService
+	{
+        void ApplyConfig(BaseConfig config);
+
+    }
+}
+
---
+++ ClientObserver/Models/Interfaces/Clients/ClientService/IClientService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using ClientObserver.Models.Server.Core.Configs;
+
+namespace ClientObserver.Models.Interfaces.Clients
+{
+    /// <summary>
+    /// Defines a contract for client services that require connection and configuration functionalities.
+    /// Implementing this interface ensures that a client service can connect to a server or network resource asynchronously and apply a given configuration.
+    /// </summary>
+    public interface IClientService
+    {
+        /// <summary>
+        /// Applies a specified configuration to the client service.
+        /// This method is responsible for configuring the client based on the provided <see cref="BaseConfig"/>.
+        /// </summary>
+        /// <param name="config">The configuration to apply to the client service.</param>
+        void ApplyConfig(BaseConfig config);
+        Task ConnectAsync();
+        Task DisconnectAsync();
+
+    }
+}
