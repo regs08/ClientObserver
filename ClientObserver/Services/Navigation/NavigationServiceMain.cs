9b1fd1e8c3513ef823a455e31544ef3db243a417
cregs08 <cole.regnier@gmail.com>
2024-03-26 20:59:17 +1100

    probably have to delete this because of nav issues..

--- ClientObserver/Services/Navigation/NavigationServiceMain.cs
+++ ClientObserver/Services/Navigation/NavigationServiceMain.cs
@@ -3,15 +3,17 @@ using ClientObserver.ViewModels.DeviceDisplay;
 using ClientObserver.ViewModels.ServerConnectionSetup;
 using ClientObserver.Views.Display.Server;
 using ClientObserver.Views.ServerConnectionSetup;
+using ClientObserver.Views.Display.Server.Core;
+using ClientObserver.ViewModels.DeviceDisplay.Core;
 namespace ClientObserver.Services.Navigation
 {
     public class NavigationServiceMain : NavigationServiceBase
     {
         protected override Dictionary<Type, Type> ViewModelToViewMapping { get; } = new Dictionary<Type, Type>
     {
-        { typeof(DeviceDisplayViewModel), typeof(ServerDisplayView) },
-        // Ensure the mapping is between ViewModel and View types
-        { typeof(ConnectionSetupViewModel), typeof(ConnectionSetupView) }
+        { typeof(DeviceDisplayViewModel), typeof(DeviceDisplayView) },
+        { typeof(ConnectionSetupViewModel), typeof(ConnectionSetupView) },
+        { typeof(DataStreamViewModel ), typeof(DataStreamView)}
     };
 
     }
