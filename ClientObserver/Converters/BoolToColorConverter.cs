b9783b25ba12e0e8c7235bd141c062f1691b7569
cregs08 <cole.regnier@gmail.com>
2024-03-26 21:00:50 +1100

    adding a converter

---
+++ ClientObserver/Converters/BoolToColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Globalization;
+
+namespace ClientObserver.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public Color TrueColor { get; set; }
+        public Color FalseColor { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? TrueColor : FalseColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
